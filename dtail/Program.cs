3189575f88878ceb5c9b393906246f77dc8de6ad
Pasi Savolainen <pasi.savolainen@iki.fi>
2020-10-22 20:24:26 +0300

    Humble beginnings

---
+++ dtail/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Docker.DotNet;
+
+namespace dtail
+{
+    class Program
+    {
+        static async Task Main(string[] _)
+        {
+            var dockerClient = new DockerClientConfiguration()
+                .CreateClient();
+
+            var conts = await dockerClient.Containers.ListContainersAsync(new Docker.DotNet.Models.ContainersListParameters { All = true });
+
+            Console.WriteLine($"Seeing {conts.Count} containers.");
+        }
+    }
+}
